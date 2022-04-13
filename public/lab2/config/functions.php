f7cd71be2a09a62203d2b100f2588090a46b072f
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-04-13 14:06:53 -0500

    config done

---
+++ public/lab2/config/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Load view with data
+ *
+ * @param string $view_name
+ * @param array $data
+ * @return void
+ */
+function view(string $view_name, array $data = []):void
+{
+    try {
+        extract($data);
+        // title and content now visible in this scope
+        $path = __DIR__ . '/../views/' . $view_name . '.view.php';
+        if(!file_exists($path)) {
+            throw new Exception('View ' . $path . ' not found.');
+        }
+        require($path);
+    } catch(Exception $e) {
+        echo $e->getMessage();
+        die;
+    }
+}
+
+function dd($var) {
+    echo '<pre>';
+    print_r($var);
+    die;
+}
+
+function dc()
+{
+    if(defined('ENV') && ENV !== 'production') {
+        if(func_num_args()) {
+            $out = func_get_args();
+        } else {
+            $out = $GLOBALS;
+        }
+        $json = json_encode($out);
+        echo "<script>console.log($json)</script>";
+    }
+}
