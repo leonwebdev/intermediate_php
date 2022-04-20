b685d9c0fbfde2d71277947a1e3d204e1cf6b7a4
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-04-19 19:45:08 -0500

    json func complete, need flag to force transfer object to array

--- public/lab3/inc/records.inc.php
+++ public/lab3/inc/records.inc.php
@@ -1,19 +1,21 @@
 <?php
 
-if (empty($_COOKIE['tracker'])) {
-    setcookie('tracker', '');
-}
+// if (!isset($_COOKIE['tracker'])) {
+//     setcookie('tracker', '');
+// }
 
-var_dump($_COOKIE);
+$tracker = json_decode($_COOKIE['tracker'] ?? '', JSON_OBJECT_AS_ARRAY) ?? [[
+    'title' => '',
+    'file' => '',
+]];
 
-$tracker = json_decode($_COOKIE['tracker']);
-
-var_dump($tracker);
-
-for ($i = sizeof($tracker); $i < sizeof($tracker) - 10; $i--) {
+for ($i = sizeof($tracker) - 1; $i > sizeof($tracker) - 10 && $i > 0; $i--) {
 
     echo '<p>';
-    echo 'tets';
+    // echo $i;
+    // print_r($tracker);
+    // die;
+    echo '<a class = "plain_a" href = "' . $tracker[$i]['file'] . '">' . $tracker[$i]['title'] . '</a>';
     echo '</p>';
 }
 
@@ -22,6 +24,18 @@ $push_array = [
     'file' => $file,
 ];
 
+// echo '<pre>';
+// print_r($push_array);
+
+// echo '<pre>';
+// print_r($tracker);
+
 array_push($tracker, $push_array);
 
-setcookie('tracker', json_encode($tracker));
+// echo '<pre>';
+// print_r($tracker);
+
+setcookie('tracker', json_encode($tracker, JSON_FORCE_OBJECT));
+
+// echo '<pre>';
+// print_r($_COOKIE['tracker']);
