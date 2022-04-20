1e532be3406f2e55ac286e07bcba8972c5446f72
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-04-20 14:43:49 -0500

    general query done

--- public/lab4/includes/model.php
+++ public/lab4/includes/model.php
@@ -1,4 +1,30 @@
 <?php
 
-// All database functions here
+// require config
+require __DIR__ . '/config.php';
 
+$title = 'Some Books you might like...';
+
+$query = "SELECT 
+            book.id as id,
+            book.title as title,
+            book.num_pages as num_pages,
+            book.year_published as year_published,
+            author.name as author,
+            genre.name as genre,
+            book.price as price,
+            book.description as book_description,
+            book.image as image
+            FROM 
+            book
+            INNER JOIN author ON book.author_id = author.id
+            INNER JOIN genre ON book.genre_id = genre.id
+            INNER JOIN format ON book.format_id = format.id
+            INNER JOIN publisher ON book.publisher_id = publisher.id
+            ";
+
+$stmt = $dbh->query($query);
+
+$results = $stmt->fetchAll();
+
+// var_dump($results);
